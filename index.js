21dcf4bb5540d784aea3073ace860e300af5f988
Hao Nguyen <nmhhao1996@gmail.com>
2022-09-03 17:55:10 +0700

    Add static method setBottomInset

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ const isIPad = (() => {
 
 let _customStatusBarHeight = null;
 let _customStatusBarHidden = null;
+let _customBottomInset = null;
 const statusBarHeight = isLandscape => {
   if (_customStatusBarHeight !== null) {
     return _customStatusBarHeight;
@@ -124,6 +125,10 @@ const doubleFromPercentString = percent => {
 class SafeView extends Component {
   static setStatusBarHeight = height => {
     _customStatusBarHeight = height;
+  };
+
+  static setBottomInset = height => {
+    _customBottomInset = height;
   };
 
   static setStatusBarHidden = hidden => {
@@ -357,6 +362,9 @@ export function getInset(key, isLandscape) {
       return statusBarHeight(isLandscape);
     }
     case 'bottom': {
+      if (_customBottomInset != null) {
+        return _customBottomInset;
+      }
       if (isIPhoneX) {
         return isLandscape ? 24 : 34;
       }
